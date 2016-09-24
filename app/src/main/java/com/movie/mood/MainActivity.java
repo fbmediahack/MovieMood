35db5d845f498199a2b7a6695a38873c080d12ad
Carlo Marinangeli <carlo.marinangeli@deliveroo.co.uk>
2016-09-24 12:13:04 +0100

    First working camera

--- app/src/main/java/com/movie/mood/MainActivity.java
+++ app/src/main/java/com/movie/mood/MainActivity.java
@@ -1,7 +1,10 @@
 package com.movie.mood;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.View.OnClickListener;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.button_camera).setOnClickListener(new OnClickListener() {
+            @Override public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, CameraActivity.class));
+            }
+        });
     }
 }
