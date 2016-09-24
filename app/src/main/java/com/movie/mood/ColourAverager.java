e920ff5e24fb67d6ce9770cbce75cf65eacef257
Rory Kelly <rory@rockspin.co.uk>
2016-09-24 12:31:46 +0100

    Banded color averager.

--- app/src/main/java/com/movie/mood/ColourAverager.java
+++ app/src/main/java/com/movie/mood/ColourAverager.java
@@ -32,4 +32,39 @@ public class ColourAverager {
         final int b = (int) (blueBucket / pixelCount);
         return Color.rgb(r,g, b);
     }
+
+    public int[] averagePixelsInImage(Bitmap bitmap, int bands) {
+
+        final int width = bitmap.getWidth();
+        final int height = bitmap.getHeight();
+
+        final int bandWidth = width / bands;
+
+        int[] results = new int[bands];
+
+        for (int bandIndex = 0; bandIndex < bands; bandIndex++) {
+            int bandStart = bandIndex * bandWidth;
+            int bandEnd = bandStart + bandWidth;
+
+            long redBucket = 0;
+            long greenBucket = 0;
+            long blueBucket = 0;
+            long pixelCount = 0;
+
+            for (int x = bandStart; x < bandEnd; x++) {
+                for (int y = 0; y < height; y++) {
+                    pixelCount++;
+                    final int c = bitmap.getPixel(x, y);
+                    redBucket += Color.red(c);
+                    greenBucket += Color.green(c);
+                    blueBucket += Color.blue(c);
+                }
+            }
+            final int r = (int) (redBucket / pixelCount);
+            final int g = (int) (greenBucket / pixelCount);
+            final int b = (int) (blueBucket / pixelCount);
+            results[bandIndex] = Color.rgb(r, g, b);
+        }
+        return results;
+    }
 }
